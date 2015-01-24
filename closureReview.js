ce70a726acc8299d881892e0786a8d4206fc9f15
ean <ea.nplatter@gmail.com>
2015-01-24 08:51:45 -0700

    Update closureReview.js

--- closureReview.js
+++ closureReview.js
@@ -15,7 +15,7 @@ var callFriend = function(){
 
 /*
 
-Write a function that accepts a function as it's only argument and returns a new function (which calls the original function that was passed in) that can only ever be executed once.
+Write a function that accepts a function as it's first argument and returns a new function (which calls the original function that was passed in) that can only ever be executed once.
 
 Once completed, add a second arguments that allows the function to be executed N number of times. After the function has been called N number of times, console.log('STAHHP');
 

bd753caa2c782bfbe73e42c1c6f5b50dfebf542a
arturoromerotcs <arturo.romerotcs@gmail.com>
2015-04-25 10:30:53 -0600

    finished get id from array of objects

--- callbackReview.js
+++ callbackReview.js
@@ -30,22 +30,22 @@ last(names, function(lastName){
 //have the contains function return a boolean value for if the name is in the array or not.
 
 
-var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-contains('Colt', names, function(yes){
-  if(yes){
-    console.log('Colt is in the array');
-  } else {
-    console.log('Colt is not in the list');
-  }
-});
+// var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+// contains('Colt', names, function(yes){
+//   if(yes){
+//     console.log('Colt is in the array');
+//   } else {
+//     console.log('Colt is not in the list');
+//   }
+// });
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-var numbers = [1,2,3,4,5];
-//Produces a new array of values by mapping each value in list through a transformation function
-map(numbers, function(num){
-  return num * 2; //returns an array of [2,4,6,8,10]
-});
+// var numbers = [1,2,3,4,5];
+// //Produces a new array of values by mapping each value in list through a transformation function
+// map(numbers, function(num){
+//   return num * 2; //returns an array of [2,4,6,8,10]
+// });
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -71,17 +71,12 @@ uniq(names, function(uniqArr){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-each(names, function(item, indice){
-  console.log('The item in the ' + indice + 'position is ' + item)
-});
-
-
-
+// var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+// each(names, function(item, indice){
+//   console.log('The item in the ' + indice + 'position is ' + item)
+// });
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
-
-
 
 
 var users = [
@@ -104,6 +99,16 @@ var users = [
     address: '192 East 32 North'
   },
 ];
+
+function getUserById(str, arr, cb){
+  for (var i = 0; i < arr.length; i++) {
+      if (arr[i].id === str){
+        cb(arr[i])
+      }
+    }
+  };
+
+
 getUserById('16t', users, function(user){
   console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
 });
@@ -116,8 +121,8 @@ getUserById('16t', users, function(user){
 
 
 
-//Looks through each value in the list, returning the first one that passes a truth test 
-var numbers  = [1, 2, 3, 4, 5, 6];
-find(numbers, function(num){ 
-  return num % 2 == 0; //should return 2
-})
+// //Looks through each value in the list, returning the first one that passes a truth test 
+// var numbers  = [1, 2, 3, 4, 5, 6];
+// find(numbers, function(num){ 
+//   return num % 2 == 0; //should return 2
+// })

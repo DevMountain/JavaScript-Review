16799d53cceb52c7720dd42fc89d78c780ee8cc6
Tony Calderon <anthonyrcalderon@gmail.com>
2015-07-11 14:06:06 -0600

    closureReview.js

--- closureReview.js
+++ closureReview.js
@@ -7,16 +7,32 @@ var callFriend = function(){
 };
 
 //Above you're given a callFriend function that returns another function. 
-//Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
+//Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' 
+//in your console.
 
   //code here
-
+var callJake = callFriend();
+callJake('435-215-9248');
 
 
 /*
 
-Write a function that accepts a function as it's first argument and returns a new function (which calls the original function that was passed in) that can only ever be executed once.
+Write a function that accepts a function as it's first argument and returns a new function 
+(which calls the original function that was passed in) that can only ever be executed once.
 
-Once completed, add a second arguments that allows the function to be executed N number of times. After the function has been called N number of times, console.log('STAHHP');
+Once completed, add a second arguments that allows the function to be executed N number of times. 
+After the function has been called N number of times, console.log('STAHHP');
 
 */
+function oneTimeOnly(funct) {
+	var once = false;
+	return function(){
+		if (!once) {
+			funct();
+			once = true;
+		}
+	}
+}
+
+
+

7b83cf2c06c4a342c9a0614e7d0d0c4f36dd01fe
cbejensen <cbejensen@gmail.com>
2015-10-09 20:46:34 -0600

    fixed type-o 'or' in math.random problem

--- arrayReview.js
+++ arrayReview.js
@@ -20,7 +20,7 @@ var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full of numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
